4aec622ecdeb4533da625a888d454c347a8b6a29
devPinheiro <samuelpinheiro41@gmail.com>
2019-12-12 12:24:29 +0100

    final checks

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -13,7 +13,7 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
+        self.database_name = "trivia_test_2"
         self.database_path = "postgres://{}:{}@{}/{}".format(
             'postgres', 'samuel40', 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
@@ -33,7 +33,6 @@ class TriviaTestCase(unittest.TestCase):
             self.db.create_all()
 
     def tearDown(self):
-        """Executed after reach test"""
         pass
 
     """
@@ -67,19 +66,19 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
 
     def test_category_non_existent_question(self):
-        res = self.client().get('/categories/23/questions')
+        res = self.client().get('/categories/2345/questions')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 422)
         self.assertEqual(data['message'], "unprocessable")
         self.assertEqual(data['success'], False)
-        
+
     def test_delete_question(self):
-        res = self.client().delete('/questions/1')
+        res = self.client().delete('/questions/4')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(data['success'], False)
+        self.assertEqual(data['success'], True)
 
     def test_delete_non_existent_question(self):
         res = self.client().delete('/questions/236')
@@ -126,32 +125,27 @@ class TriviaTestCase(unittest.TestCase):
     def test_get_quiz_non_existent(self):
         res = self.client().post('/quizzes',
                                  json={
-                                    "previous_questions": [{
-                                                "answer": "Alexander Fleming",
-                                                "category": 1,
-                                                "difficulty": 3,
-                                                "id": 271,
-                                                "question": "Who penicillin?"
-                                            }],  
-                                    "quiz_category": 2
-                                    })
+                                     "previous_questions": [],
+                                     "quiz_category": 56
+                                 })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 400)
         self.assertEqual(data['success'], False)
-    
+
     def test_get_quiz(self):
         res = self.client().post('/quizzes',
                                  json={
-                                    "previous_questions": [{
-                                                "answer": "Alexander Fleming",
-                                                "category": 1,
-                                                "difficulty": 3,
-                                                "id": 1,
-                                                "question": "Who discovered?"
-                                            }],  
-                                    "quiz_category": 1
-                                    })
+                                     "previous_questions": [{
+                                         "answer": "One",
+                                         "category": 2,
+                                         "difficulty": 4,
+                                         "id": 18,
+                                         "question": "How many paintings did "
+                                         + "Van Gogh sell in his lifetime?"
+                                     }],
+                                     "quiz_category": 2
+                                 })
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
